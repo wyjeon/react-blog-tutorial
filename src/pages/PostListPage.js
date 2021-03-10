989c3ea58d06910a925e5e0ec9c19fc294091082
wyjeon <wyjeon.info@gmail.com>
2021-03-10 16:45:52 +0900

    헤더 컴포넌트 만들기

--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import Button from '../components/common/Button';
+import Header from '../components/common/Header';
 
 function PostListPage() {
   return (
-    <div>
-      <Button>버튼</Button>
-    </div>
+    <>
+      <Header />
+      <div>안녕</div>
+    </>
   );
 }
 
