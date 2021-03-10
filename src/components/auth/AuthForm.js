82050e9d9d1a934030ab9b631344a140a252b3c8
wyjeon <wyjeon.info@gmail.com>
2021-03-10 13:38:48 +0900

    리덕스로 폼 상태 관리하기

--- src/components/auth/AuthForm.js
+++ src/components/auth/AuthForm.js
@@ -67,22 +67,26 @@ const textMap = {
   register: '회원가입',
 };
 
-function AuthForm({ type }) {
+function AuthForm({ type, form, onChange, onSubmit }) {
   const text = textMap[type];
   return (
     <AuthFormBlock>
       <h3>{text}</h3>
-      <form onSubmit>
+      <form onSubmit={onSubmit}>
         <StyledInput
           autoComplete="username"
           name="username"
           placeholder="아이디"
+          onChange={onChange}
+          value={form.username}
         />
         <StyledInput
           autoComplete="new-password"
           name="password"
           placeholder="비밀번호"
           type="password"
+          onChange={onChange}
+          value={form.password}
         />
         {type === 'register' && (
           <StyledInput
@@ -90,6 +94,8 @@ function AuthForm({ type }) {
             name="passwordConfirm"
             placeholder="비밀번호 확인"
             type="password"
+            onChange={onChange}
+            value={form.passwordConfirm}
           />
         )}
 
