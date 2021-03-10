efc236780273e390419b5e9070d7180700b33289
wyjeon <wyjeon.info@gmail.com>
2021-03-10 14:34:36 +0900

    회원가입 구현

--- src/containers/auth/RegisterForm.js
+++ src/containers/auth/RegisterForm.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { withRouter } from 'react-router';
 import AuthForm from '../../components/auth/AuthForm';
-import { changeField, initalzedForm } from '../../modules/auth';
+import { changeField, initalzedForm, register } from '../../modules/auth';
+import { check } from '../../modules/user';
 
-function RegisterForm() {
+function RegisterForm({ history }) {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
 
   // 인풋 변경 이벤트 핸들러
@@ -24,12 +29,38 @@ function RegisterForm() {
   // 폼 등록 이벤트 핸들러
   const onSubmit = e => {
     e.preventDefault();
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
   // 컴포넌트가 처음 렌더링될 때 form을 초기화함
   useEffect(() => {
     dispatch(initalzedForm('register'));
   }, [dispatch]);
+
+  // 회원가입 성공/실패 처리
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('회원가입 성공');
+      console.log(auth);
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  // user 값이 잘 설정되었는지 확인
+  useEffect(() => {
+    if (user) {
+      history.push('/'); // 홈 화면으로 이동
+    }
+  }, [history, user]);
 
   return (
     <AuthForm
@@ -41,4 +72,4 @@ function RegisterForm() {
   );
 }
 
-export default RegisterForm;
+export default withRouter(RegisterForm);
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,18 @@
+import { all } from '@redux-saga/core/effects';
 import { combineReducers } from 'redux';
-import auth from './auth';
+import auth, { authSaga } from './auth';
+import loading from './loading';
+import user from './user';
+import { userSaga } from './user';
 
 const rootReducer = combineReducers({
   auth,
+  loading,
+  user,
 });
+
+export function* rootSaga() {
+  yield all([authSaga(), userSaga()]);
+}
 
 export default rootReducer;
