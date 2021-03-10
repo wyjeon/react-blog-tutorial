9feff0b62ffe1ace99a45255a68cb01dc27c99e1
wyjeon <wyjeon.info@gmail.com>
2021-03-10 14:42:38 +0900

    로그인 구현

--- src/containers/auth/LoginForm.js
+++ src/containers/auth/LoginForm.js
@@ -1,12 +1,17 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { withRouter } from 'react-router';
 import AuthForm from '../../components/auth/AuthForm';
-import { changeField, initalzedForm } from '../../modules/auth';
+import { changeField, initalzedForm, login } from '../../modules/auth';
+import { check } from '../../modules/user';
 
-function LoginForm() {
+function LoginForm({ history }) {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.login,
+    auth: auth.auth,
+    authError: auth.authError,
+    user: user.user,
   }));
 
   // 인풋 변경 이벤트 핸들러
@@ -24,7 +29,27 @@ function LoginForm() {
   // 폼 등록 이벤트 핸들러
   const onSubmit = e => {
     e.preventDefault();
+    const { username, password } = form;
+    dispatch(login({ username, password }));
   };
+
+  useEffect(() => {
+    if (authError) {
+      console.log('오류발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('로그인 성공');
+      dispatch(check());
+    }
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      history.push('/');
+    }
+  }, [history, user]);
 
   // 컴포넌트가 처음 렌더링될 때 form을 초기화함
   useEffect(() => {
@@ -41,4 +66,4 @@ function LoginForm() {
   );
 }
 
-export default LoginForm;
+export default withRouter(LoginForm);
