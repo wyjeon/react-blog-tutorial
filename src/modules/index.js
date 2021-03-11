e6b8a4e66b49414a2093a3d97ef249a375fd529c
wyjeon <wyjeon.info@gmail.com>
2021-03-11 14:57:39 +0900

    포스트 조회 기능 구현하기

--- src/modules/index.js
+++ src/modules/index.js
@@ -6,6 +6,7 @@ import user from './user';
 import write, { writeSaga } from './write';
 import post, { postSaga } from './post';
 import { userSaga } from './user';
+import posts, { postsSaga } from './posts';
 
 const rootReducer = combineReducers({
   auth,
@@ -13,10 +14,11 @@ const rootReducer = combineReducers({
   user,
   write,
   post,
+  posts,
 });
 
 export function* rootSaga() {
-  yield all([authSaga(), userSaga(), writeSaga(), postSaga()]);
+  yield all([authSaga(), userSaga(), writeSaga(), postSaga(), postsSaga()]);
 }
 
 export default rootReducer;
--- src/pages/PostListPage.js
+++ src/pages/PostListPage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import HeaderContainer from '../containers/common/HeaderContainer';
+import PaginationContainer from '../containers/posts/PaginationContainer';
+import PostListContainer from '../containers/posts/PostListContainer';
 
 function PostListPage() {
   return (
     <>
       <HeaderContainer />
-      <div>안녕</div>
+      <PostListContainer />
+      <PaginationContainer />
     </>
   );
 }
